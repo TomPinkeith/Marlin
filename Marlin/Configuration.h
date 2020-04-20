33f85dc67df4045c46b774596e3f175a36ba189f
Alessio De Benedetto <alessio.debenedetto@prxm.it>
2020-04-20 16:52:05 +0200

    Centered the bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -887,12 +887,12 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -33
-#define Y_MIN_POS -10
+#define X_MIN_POS -X_BED_SIZE / 2
+#define Y_MIN_POS -Y_BED_SIZE / 2
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 240
+#define X_MAX_POS X_BED_SIZE / 2
+#define Y_MAX_POS Y_BED_SIZE / 2
+#define Z_MAX_POS 200
 
 /**
  * Software Endstops
@@ -1133,8 +1133,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS -158
+#define MANUAL_Y_HOME_POS -102
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
