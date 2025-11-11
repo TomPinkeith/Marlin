47f74f99300cc5e4a455c2c8996cdb3521334b25
Alessio De Benedetto <alessiodebenedetto85@gmail.com>
2025-11-11 14:06:12 +0100

    Versione 2.0.5.3i = Changed Extruder 0 Pin (Broken pin on board)

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.5.3"
+//#define SHORT_BUILD_VERSION "2.0.5.3i"
 
 /**
  * Verbose version identifier which should contain a reference to the location
